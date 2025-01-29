137d3090dbe6c2bb8a6d4f1889f01927530bb383
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-29 12:47:43 -0500

    Directiva each (foreach) de thymeleaf

--- src/main/java/com/maicoldev/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/maicoldev/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.maicoldev.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.maicoldev.curso.springboot.webapp.springboot_web.models.User;
@@ -17,5 +21,18 @@ public class UserController {
         model.addAttribute("user", user);
 
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = Arrays.asList(
+                new User("Pepa", "Gonzalez"),
+                new User("Lalo", "Perez", "lalo@lalo"),
+                new User("Juanita", "Roe", "juanita@juanita"),
+                new User("Andres", "Doe"));
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "User List");
+        return "list";
     }
 }
--- src/main/java/com/maicoldev/curso/springboot/webapp/springboot_web/models/User.java
+++ src/main/java/com/maicoldev/curso/springboot/webapp/springboot_web/models/User.java
@@ -5,6 +5,15 @@ public class User {
     private String lastname;
     private String email;
 
+    public User() {
+        // Empty constructor
+    }
+
+    public User(String name, String lastname, String email) {
+        this(name, lastname);
+        this.email = email;
+    }
+
     public User(String name, String lastname) {
         this.name = name;
         this.lastname = lastname;
